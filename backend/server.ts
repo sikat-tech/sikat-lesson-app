f634179876d2edc006023ffdebb5fe99dc4756cc
!julius <julius.salinas@sikat.com>
2026-06-10 08:51:05 +0800

    ts setup

--- backend/server.ts
+++ backend/server.ts
@@ -1,30 +1,5 @@
-const net = require('net');
-const createClient = require('../frontend/client.js');
-const PORT = 3000;
-const HOST = '';
+import net from 'net';
 
-
-
-
-const server = net.createServer((socket) => {
-    
-    console.log('Client connected');
-
-    socket.on('error', (err) => {
-        console.error('Socket error:', err.message);
-    });
-
-    socket.on('end', () => {
-        console.log('Client disconnected');
-    });
-
-    socket.on('data', (data) => {
-        console.log(`Received from client: ${data}`);
-        socket.write(`Server received: ${data}`);
-    });
-});
-
-server.listen(PORT, () => {
-    createClient(PORT);
-    console.log(`Server listening on port ${PORT}`);
+const server = net.createServer(() => {
+   console.log('Client connected');
 });
