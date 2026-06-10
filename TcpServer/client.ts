08c078c8f21f0fe6a69e80541945d1ba8c54c35d
Jhonn Michael Bretaña <jhonnbretz@gmail.com>
2026-06-10 13:54:55 +0800

    create

--- TcpServer/client.ts
+++ TcpServer/client.ts
@@ -1,13 +1,72 @@
-import net from "net"
+import net from "net";
+import readline from "readline";
 
-const client = net.createConnection({ port: 8080 }, () => {
-    console.log("Connected to server");
+//iconnect sa server
+const client = net.createConnection(
+  { port: 8080, host: "127.0.0.1" },
+  () => {},
+);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-client.on("data", (data) => {
-    console.log(`Received data from server: ${data}`);
+//dito gumamit ako ng promisified method to convert callback style into promise style para mas madali gamitin sa async/await
+const ask = (q: string): Promise<string> =>
+  new Promise((resolve) => rl.question(q, resolve));
+
+
+//
+function sendRecord(Record: any): Promise<any> {
+  return new Promise((resolve) => {
+    client.once("data", (data) => {
+      resolve(data.toString());
+    });
+
+    client.write(JSON.stringify(Record));
+  });
+}
+
+async function showMenu() {
+  console.log("Menu:");
+  console.log("1. Create a Lesson");
+  console.log("2. Exit");
+
+  const choice = await ask("Enter your choice: ");
+
+  if (choice === "1") {
+    const title = await ask("Enter lesson title: ");
+    const description = await ask("Enter lesson description: ");
+
+    const record = sendRecord({
+      type: "create_lesson",
+      title,
+      description,
+    });
+    console.log("Record sent to server, waiting for response...");
+
+    await showMenu();
+  }
+
+  else if (choice === "2") {
+  rl.close();
+  client.end();
+  return;
+  }
+
+  await showMenu();
+}
+
+client.on("connect", () => {
+  console.log("Connected to TCP server");
+  showMenu();
 });
 
-client.on("close", () => {
-    console.log("Disconnected from server");
+client.on("error", (err) => {
+  console.error(`Connection error: ${err.message}`);
+});
+
+client.on("end", () => {
+  console.log("Disconnected from TCP server");
 });
--- TcpServer/server.ts
+++ TcpServer/server.ts
@@ -1,10 +1,49 @@
 import net from "net";
+import fs from "fs";
+
+const filePath = "lessons.ndjson";
+
+const PORT = 8080;
+const HOST = "127.0.0.1";
+
+function lessonToNDJSON(title: string, description: string) {
+  const lesson = {
+    title,
+    description,
+  };
+
+  fs.appendFileSync(filePath, JSON.stringify(lesson) + "\n");
+}
+
+function handleClientData(msg: any) {
+  if (msg.type === "create_lesson") {
+    lessonToNDJSON(msg.title, msg.description);
+
+    return {
+      ok: true,
+      status: "success",
+      message: "Lesson created successfully",
+    };
+  } else {
+    return {
+      ok: false,
+      status: "error",
+      message: "Invalid request type",
+    };
+  }
+}
 
 const server = net.createServer((socket) => {
   console.log("Client connected");
 
   socket.on("data", (data) => {
+
     console.log(`Received data from client: ${data}`);
+
+    const msg = JSON.parse(data.toString()); // Assuming the client sends JSON data
+    const response = handleClientData(msg); // Process the message and generate a response
+    socket.write(JSON.stringify(response)); // covert the response to JSON and send it back to the client
+
   });
 
   socket.on("end", () => {
@@ -12,6 +51,6 @@ const server = net.createServer((socket) => {
   });
 });
 
-server.listen(8080, () => {
-  console.log("TCP server listening on port 8080");
+server.listen(PORT, HOST, () => {
+  console.log(`TCP server listening on port ${PORT} at ${HOST}`);
 });
